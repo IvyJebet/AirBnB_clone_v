bf12be7a74d671782d41b3cfb6343816254e01af
IvyJebet <jebetivy388@gmail.com>
2024-02-08 06:30:49 -0800

    BaseModel File update

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,22 @@ from datetime import datetime
 
 class BaseModel:
     """Base class for all the classes in the project"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
+    def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key in ("created_at", "updated_at"):
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
 
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
         """Updates updated_at with current datetime"""
