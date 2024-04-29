18dbfe017394a817a8890f8d01ceb17e39a08300
JebetIvy <138754525+IvyJebet@users.noreply.github.com>
2024-04-29 07:30:17 +0300

    User.py File

--- models/user.py
+++ models/user.py
@@ -1,10 +1,18 @@
 #!usr/bin/python3
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """Creates a new user"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    __tablename__ = "users"
+
+    email = Column("email", String(128), nullable=False)
+    password = Column("password", String(128), nullable=False)
+    first_name = Column("first_name", String(128))
+    last_name = Column("last_name", String(128))
+    places = relationship(
+        "Place", backref="user", cascade="all, delete, delete-orphan")
+    reviews = relationship(
+        "Review", backref="user", cascade="all, delete, delete-orphan")
