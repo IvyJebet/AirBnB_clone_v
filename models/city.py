5337e45de1d9e921000e59f7bbbfb1fa512993bf
JebetIvy <138754525+IvyJebet@users.noreply.github.com>
2024-04-29 07:23:25 +0300

    City.py File

--- models/city.py
+++ models/city.py
@@ -4,15 +4,17 @@
 It defines one class, `City(),
 which sub-classes the `BaseModel()` class.`
 """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """A city in the application.
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = "cities"
 
-    Attributes:
-        name
-        state_id
-    """
-    name = ""
-    state_id = ""
+    name = Column('name', String(128), nullable=False)
+    state_id = Column(
+        'state_id', String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship(
+        'Place', backref="cities", cascade="all, delete, delete-orphan")
